625d7040ae6cdac170bfe6a5da18ae45cef38bfc
Sagar Bera <86586277+sboy99@users.noreply.github.com>
2023-04-17 15:06:32 +0530

    feat(init): initailize prisma module (#2)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './resources/prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+  ],
   controllers: [],
   providers: [],
 })
