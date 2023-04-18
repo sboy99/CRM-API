f7b7ddf60d38c04c6b6efd1f93a5b915af9f5abb
Sagar Bera <contact.sagarbera@gmail.com>
2023-04-18 18:32:17 +0530

    feat: implement mail module

---
+++ src/resources/mail/mail.module.ts
@@ -0,0 +1,33 @@
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { Module } from '@nestjs/common';
+import { join } from 'path';
+import { MailService } from './mail.service';
+
+@Module({
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_USER_PASSWORD,
+        },
+      },
+      defaults: {
+        from: '"No Reply" <noreply@crm.com>',
+      },
+      template: {
+        dir: join(process.cwd(), 'templates'),
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
+  providers: [MailService],
+  exports: [MailService],
+})
+export class MailModule {}
